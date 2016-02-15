a4c1d602cbca77fd38485fbf1c52a68fbcbb3ed0
mellis <mellis@westmarkproducts.com>
2016-02-15 08:07:19 -0800

    Completed update form and update logic

--- application.py
+++ application.py
@@ -57,13 +57,43 @@ def new_job():
 @app.route('/job/edit/<int:job_id>', methods=['GET', 'POST'])
 def edit_job(job_id):
     if request.method == 'POST':
-        #update code here
+        #set date values
+        appliedOnDate = None
+        firstInterviewDate = None
+        secondInterviewDate = None
+        thirdInterviewDate = None
+        if request.form['applied_on'] != '':
+            appliedOnDate = datetime.datetime.strptime(request.form['applied_on'], '%Y-%m-%d').date()
+        if request.form['first_interview'] != '':
+            firstInterviewDate = datetime.datetime.strptime(request.form['first_interview'], '%Y-%m-%d').date()
+        if request.form['second_interview'] != '':
+            secondInterviewDate = datetime.datetime.strptime(request.form['second_interview'], '%Y-%m-%d').date()
+        if request.form['third_interview'] != '':
+            thirdInterviewDate = datetime.datetime.strptime(request.form['third_interview'], '%Y-%m-%d').date()
+        
+        #update record
+        job = session.query(Jobs).filter_by(job_id=job_id).one()
+        job.job_title = request.form['job_title']
+        job.job_url = request.form['job_url']
+        job.company_name = request.form['company_name']
+        job.company_url = request.form['company_url']
+        job.company_phone = request.form['company_phone']
+        job.applied_on = appliedOnDate
+        job.cover_page = request.form['cover_page']
+        job.notes = request.form['notes']
+        job.first_interview = firstInterviewDate
+        job.second_interview = secondInterviewDate
+        job.third_interview = thirdInterviewDate
+        session.commit()
         return redirect(url_for('show_jobs'))
     else:
         job = session.query(Jobs).filter_by(job_id=job_id).one()
         return render_template('edit_job.html', job_id=job_id, job_title=job.job_title, 
             job_url=job.job_url, company_name=job.company_name, company_url=job.company_url,
-            company_phone=job.company_phone, company_contact=job.company_contact)
+            company_phone=job.company_phone, company_contact=job.company_contact, 
+            applied_on=job.applied_on, cover_page=job.cover_page, notes=job.notes,
+            first_interview=job.first_interview, second_interview=job.second_interview,
+            third_interview=job.third_interview)
         
 if __name__ == '__main__':
     app.debug = True
